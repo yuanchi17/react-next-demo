a689a3386e56a9b6efaeabdacf8f7a27150f8aaa
李元琦 <yuan.chi@program.com.tw>
2023-08-29 13:13:38 +0800

    feat: usePathname

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,32 @@
+'use client'
+
 import Link from 'next/link'
 
+// @ts-ignore
+import { usePathname } from 'next/navigation'
+
 export default function Header() {
+  const pathname = usePathname()
+
   const navs = [
     { title: 'Home', href: '/' },
     { title: 'Post', href: '/post' },
     { title: 'Demo', href: '/demo' },
   ]
+
   return (
     <div>
-      {navs.map((nav, id) => (
-        <>
-          {!id ? <></> : <span>｜</span>}
-          <Link href={nav.href} key={id}>{nav.title}</Link>
-        </>
-      ))}
+      {navs.map((nav, id) => {
+        let isActive
+        if (nav.href === '/') isActive = pathname === nav.href
+        else isActive = pathname.startsWith(nav.href)
+
+        return (
+          <>
+            {!id ? <></> : <span>｜</span>}
+            <Link href={nav.href} key={id} style={{ color: isActive ? 'blue' : ''}}>{nav.title}</Link>
+          </>)
+      })}
     </div>
   )
 }
